e296c48d3a4c797cb4031e6b9da1197deed6d139
Brendan Allan <brendonovich@outlook.com>
2024-10-01 18:46:31 +0800

    start withSolidBase wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log("SolidBase");
+import type { SolidStartInlineConfig } from "@solidjs/start/config";
+// @ts-expect-error
+import mdx from "@vinxi/plugin-mdx";
+
+export type SolidBaseConfig = {};
+
+export function withSolidBase(
+  startConfig: SolidStartInlineConfig,
+  solidBaseConfig: SolidBaseConfig,
+) {
+  startConfig.extensions = [
+    ...new Set((startConfig.extensions ?? []).concat(["ts", "tsx"])),
+  ];
+
+  const vite = startConfig.vite;
+  startConfig.vite = (options) => {
+    const viteConfig = typeof vite === "function" ? vite(options) : vite ?? {};
+
+    viteConfig.plugins ??= [];
+    viteConfig.plugins.push(
+      mdx.default.withImports({})({
+        jsx: true,
+        jsxImportSource: "solid-js",
+        providerImportSource: "solid-mdx",
+      }),
+    );
+
+    return viteConfig;
+  };
+
+  return startConfig;
+}
