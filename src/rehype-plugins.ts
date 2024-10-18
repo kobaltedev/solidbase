1a1ea6280e5c67b02a5383eb5f25498169bc5659
Brendan Allan <brendonovich@outlook.com>
2024-10-19 01:46:12 +1100

    fix: use css naming for mdx style attributes

--- src/rehype-plugins.ts
+++ src/rehype-plugins.ts
@@ -4,38 +4,38 @@ const DANGEROUSLY_SET_INNER_HTML = "dangerouslySetInnerHTML";
 
 // rehype-expressive-code is hardcoded to use dangerouslySetInnerHtml
 export function rehypeFixExpressiveCodeJsx() {
-  return (tree: any) => {
-    visit(
-      tree,
-      "mdxJsxFlowElement",
-      (node: { attributes?: any[]; name: string }, index, parent) => {
-        const dangrouslySetInnerHtmlAttribute = node.attributes?.find(
-          (a) => a.name === DANGEROUSLY_SET_INNER_HTML,
-        );
-        if (!dangrouslySetInnerHtmlAttribute || index === undefined) return;
+	return (tree: any) => {
+		visit(
+			tree,
+			"mdxJsxFlowElement",
+			(node: { attributes?: any[]; name: string }, index, parent) => {
+				const dangrouslySetInnerHtmlAttribute = node.attributes?.find(
+					(a) => a.name === DANGEROUSLY_SET_INNER_HTML,
+				);
+				if (!dangrouslySetInnerHtmlAttribute || index === undefined) return;
 
-        const innerHTML =
-          dangrouslySetInnerHtmlAttribute.value.data.estree.body[0].expression
-            .properties[0].value.value;
+				const innerHTML =
+					dangrouslySetInnerHtmlAttribute.value.data.estree.body[0].expression
+						.properties[0].value.value;
 
-        parent.children[index] = {
-          type: "element",
-          tagName: node.name,
-          properties: node.attributes
-            ?.filter((n) => n.name !== DANGEROUSLY_SET_INNER_HTML)
-            ?.reduce(
-              (acc, attr) => {
-                acc[attr.name] = attr.value;
-                return acc;
-              },
-              {
-                // solid inserts this as the raw element content,
-                // using children results in utf-8 encoding eg. &amp
-                innerHTML,
-              },
-            ),
-        };
-      },
-    );
-  };
+				parent.children[index] = {
+					type: "element",
+					tagName: node.name,
+					properties: node.attributes
+						?.filter((n) => n.name !== DANGEROUSLY_SET_INNER_HTML)
+						?.reduce(
+							(acc, attr) => {
+								acc[attr.name] = attr.value;
+								return acc;
+							},
+							{
+								// solid inserts this as the raw element content,
+								// using children results in utf-8 encoding eg. &amp
+								innerHTML,
+							},
+						),
+				};
+			},
+		);
+	};
 }
