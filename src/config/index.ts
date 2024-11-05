372ff22619fba0d193c21b89ca1d446b9677b305
Brendan Allan <brendonovich@outlook.com>
2024-11-05 15:12:23 +0800

    fix: distribute config as esmodule

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,125 +1,125 @@
 import type {
-	SolidStartInlineConfig,
-	ViteCustomizableConfig,
+  SolidStartInlineConfig,
+  ViteCustomizableConfig,
 } from "@solidjs/start/config";
 import type { Plugin, Plugin as VitePlugin } from "vite";
 
-import defaultTheme from "../default-theme";
-import { solidBaseMdx } from "./mdx";
-import solidBaseVitePlugin from "./vite-plugin";
+import defaultTheme from "../default-theme/index.js";
+import { solidBaseMdx } from "./mdx.js";
+import solidBaseVitePlugin from "./vite-plugin/index.js";
 
 export type SolidBaseConfig<ThemeConfig> = {
-	title?: string;
-	description?: string;
-	logo?: string;
-	titleTemplate?: string;
-	issueAutolink?: IssueAutoLinkConfig;
-	lang?: string;
-	locales?: Record<string, LocaleConfig<ThemeConfig>>;
-	themeConfig?: ThemeConfig;
-	editPath?: string | ((path: string) => string);
-	lastUpdated?: Intl.DateTimeFormatOptions | false;
-	markdown?: {
-		expressiveCode?: RehypeExpressiveCodeOptions;
-		remarkPlugins?: PluggableList;
-		rehypePlugins?: PluggableList;
-	};
+  title?: string;
+  description?: string;
+  logo?: string;
+  titleTemplate?: string;
+  issueAutolink?: IssueAutoLinkConfig;
+  lang?: string;
+  locales?: Record<string, LocaleConfig<ThemeConfig>>;
+  themeConfig?: ThemeConfig;
+  editPath?: string | ((path: string) => string);
+  lastUpdated?: Intl.DateTimeFormatOptions | false;
+  markdown?: {
+    expressiveCode?: RehypeExpressiveCodeOptions;
+    remarkPlugins?: PluggableList;
+    rehypePlugins?: PluggableList;
+  };
 };
 
 type ResolvedConfigKeys =
-	| "title"
-	| "description"
-	| "lang"
-	| "issueAutolink"
-	| "lastUpdated";
+  | "title"
+  | "description"
+  | "lang"
+  | "issueAutolink"
+  | "lastUpdated";
 
 export type SolidBaseResolvedConfig<ThemeConfig> = Omit<
-	SolidBaseConfig<ThemeConfig>,
-	ResolvedConfigKeys
+  SolidBaseConfig<ThemeConfig>,
+  ResolvedConfigKeys
 > &
-	Required<Pick<SolidBaseConfig<ThemeConfig>, ResolvedConfigKeys>>;
+  Required<Pick<SolidBaseConfig<ThemeConfig>, ResolvedConfigKeys>>;
 
 export type LocaleConfig<ThemeConfig> = {
-	label: string;
-	lang?: string;
-	link?: string;
-	themeConfig?: ThemeConfig;
+  label: string;
+  lang?: string;
+  link?: string;
+  themeConfig?: ThemeConfig;
 };
 
 export type ThemeDefinition<Config> = {
-	componentsPath: string;
-	extends?: ThemeDefinition<Config>;
-	vite?(config: SolidBaseResolvedConfig<Config>): Omit<VitePlugin, "name">;
+  componentsPath: string;
+  extends?: ThemeDefinition<Config>;
+  vite?(config: SolidBaseResolvedConfig<Config>): Omit<VitePlugin, "name">;
 };
 
 export const withSolidBase = createWithSolidBase(defaultTheme);
 
 export function createWithSolidBase<ThemeConfig>(
-	theme: ThemeDefinition<ThemeConfig>,
+  theme: ThemeDefinition<ThemeConfig>,
 ) {
-	if (parse(theme.componentsPath).ext !== "") {
-		theme.componentsPath = dirname(theme.componentsPath);
-	}
+  if (parse(theme.componentsPath).ext !== "") {
+    theme.componentsPath = dirname(theme.componentsPath);
+  }
 
-	return (
-		startConfig?: SolidStartInlineConfig,
-		solidBaseConfig?: SolidBaseConfig<ThemeConfig>,
-	) => {
-		const config = startConfig ?? {};
+  return (
+    startConfig?: SolidStartInlineConfig,
+    solidBaseConfig?: SolidBaseConfig<ThemeConfig>,
+  ) => {
+    const config = startConfig ?? {};
 
-		process.env.PORT ??= "4000";
+    process.env.PORT ??= "4000";
 
-		config.extensions = [
-			...new Set((config.extensions ?? []).concat(["md", "mdx"])),
-		];
-		config.server ??= {};
-		config.server.prerender ??= {
-			crawlLinks: true,
-		};
+    config.extensions = [
+      ...new Set((config.extensions ?? []).concat(["md", "mdx"])),
+    ];
+    config.server ??= {};
+    config.server.prerender ??= {
+      crawlLinks: true,
+    };
 
-		const sbConfig: SolidBaseResolvedConfig<ThemeConfig> = {
-			title: "SolidBase",
-			description: "Solid Start Powered Static Site Generator",
-			lang: "en-US",
-			issueAutolink: false,
-			lastUpdated: { dateStyle: "short", timeStyle: "short" },
-			...solidBaseConfig,
-		};
+    const sbConfig: SolidBaseResolvedConfig<ThemeConfig> = {
+      title: "SolidBase",
+      description: "Solid Start Powered Static Site Generator",
+      lang: "en-US",
+      issueAutolink: false,
+      lastUpdated: { dateStyle: "short", timeStyle: "short" },
+      ...solidBaseConfig,
+    };
 
-		const vite = config.vite;
+    const vite = config.vite;
 
-		config.vite = (options) => {
-			const viteConfig =
-				typeof vite === "function"
-					? vite(options)
-					: { ...(vite ?? ({} as ViteCustomizableConfig)) };
+    config.vite = (options) => {
+      const viteConfig =
+        typeof vite === "function"
+          ? vite(options)
+          : { ...(vite ?? ({} as ViteCustomizableConfig)) };
 
-			((viteConfig.optimizeDeps ??= {}).exclude ??= []).push("fsevents");
+      ((viteConfig.optimizeDeps ??= {}).exclude ??= []).push("fsevents");
 
-			viteConfig.plugins = [...(viteConfig.plugins ?? [])];
-			viteConfig.plugins.push(solidBaseMdx(sbConfig));
-			viteConfig.plugins.push(solidBaseVitePlugin(theme, config, sbConfig));
+      viteConfig.plugins = [...(viteConfig.plugins ?? [])];
+      viteConfig.plugins.push(solidBaseMdx(sbConfig));
+      viteConfig.plugins.push(solidBaseVitePlugin(theme, config, sbConfig));
 
-			let t: ThemeDefinition<any> | undefined = theme;
-			const plugins: Array<Plugin> = [];
-			while (t !== undefined) {
-				if (t.vite)
-					plugins.push({
-						...t.vite(sbConfig),
-						name: `solidbase-theme-${plugins.length}`,
-					});
+      let t: ThemeDefinition<any> | undefined = theme;
+      const plugins: Array<Plugin> = [];
+      while (t !== undefined) {
+        if (t.vite)
+          plugins.push({
+            ...t.vite(sbConfig),
+            name: `solidbase-theme-${plugins.length}`,
+          });
 
-				t = t.extends;
-			}
-			plugins.reverse();
+        t = t.extends;
+      }
+      plugins.reverse();
 
-			viteConfig.plugins.push(...plugins);
+      viteConfig.plugins.push(...plugins);
 
-			return viteConfig;
-		};
+      return viteConfig;
+    };
 
-		return config;
-	};
+    return config;
+  };
 }
 
 import { dirname, parse } from "node:path";
@@ -127,6 +127,6 @@ import type { RehypeExpressiveCodeOptions } from "rehype-expressive-code";
 import type { PluggableList } from "unified";
 import type { IssueAutoLinkConfig } from "./remark-plugins";
 export function defineTheme<C>(def: ThemeDefinition<C>) {
-	return def;
+  return def;
 }
 export type Theme<C> = ReturnType<typeof defineTheme<C>>;
--- src/config/vite-plugin/index.ts
+++ src/config/vite-plugin/index.ts
@@ -3,54 +3,54 @@ import { fileURLToPath } from "node:url";
 import type { SolidStartInlineConfig } from "@solidjs/start/config";
 import type { PluginOption } from "vite";
 
-import type { SolidBaseConfig, ThemeDefinition } from "../";
-import { loadVirtual, transformMdxModule } from "./virtual";
+import type { SolidBaseConfig, ThemeDefinition } from "../index.js";
+import { loadVirtual, transformMdxModule } from "./virtual.js";
 
 const virtualModuleId = "virtual:solidbase";
 const resolvedVirtualModuleId = `\0${virtualModuleId}`;
 
 export default function solidBaseVitePlugin(
-	theme: ThemeDefinition<any>,
-	startConfig: SolidStartInlineConfig,
-	solidBaseConfig: Partial<SolidBaseConfig<any>>,
+  theme: ThemeDefinition<any>,
+  startConfig: SolidStartInlineConfig,
+  solidBaseConfig: Partial<SolidBaseConfig<any>>,
 ): PluginOption {
-	return [
-		{
-			name: "solidbase:pre",
-			enforce: "pre",
-			resolveId(id) {
-				if (id === virtualModuleId) {
-					return resolvedVirtualModuleId;
-				}
-			},
-			async load(id) {
-				if (id === resolvedVirtualModuleId) {
-					return loadVirtual(theme, startConfig, solidBaseConfig);
-				}
-			},
-			transform(code, id) {
-				if (isMarkdown(id)) {
-					return code.replaceAll(
-						/="(\$\$SolidBase_RelativeImport\d+)"/gm,
-						(_, ident) => `={${ident}}`,
-					);
-				}
-			},
-		},
-		{
-			name: "solidbase:post",
-			enforce: "post",
-			transform(code, id) {
-				if (
-					id.startsWith(dirname(fileURLToPath(import.meta.url))) &&
-					isMarkdown(id)
-				)
-					return transformMdxModule(code, id, startConfig, solidBaseConfig);
-			},
-		},
-	];
+  return [
+    {
+      name: "solidbase:pre",
+      enforce: "pre",
+      resolveId(id) {
+        if (id === virtualModuleId) {
+          return resolvedVirtualModuleId;
+        }
+      },
+      async load(id) {
+        if (id === resolvedVirtualModuleId) {
+          return loadVirtual(theme, startConfig, solidBaseConfig);
+        }
+      },
+      transform(code, id) {
+        if (isMarkdown(id)) {
+          return code.replaceAll(
+            /="(\$\$SolidBase_RelativeImport\d+)"/gm,
+            (_, ident) => `={${ident}}`,
+          );
+        }
+      },
+    },
+    {
+      name: "solidbase:post",
+      enforce: "post",
+      transform(code, id) {
+        if (
+          id.startsWith(dirname(fileURLToPath(import.meta.url))) &&
+          isMarkdown(id)
+        )
+          return transformMdxModule(code, id, startConfig, solidBaseConfig);
+      },
+    },
+  ];
 }
 
 export function isMarkdown(path: string) {
-	return !!path.match(/\.(mdx|md)/gm);
+  return !!path.match(/\.(mdx|md)/gm);
 }
