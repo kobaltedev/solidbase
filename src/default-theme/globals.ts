0d8eb1fbf1ce3500636cc78a4b1cd70e587ba043
Brendan Allan <brendonovich@outlook.com>
2025-04-03 20:48:23 +0800

    fix: only setTimeout on client (#73)

--- src/default-theme/globals.ts
+++ src/default-theme/globals.ts
@@ -1,13 +1,17 @@
 import { createMediaQuery } from "@solid-primitives/media";
-import { createEffect, createRoot, createSignal, on } from "solid-js";
+import { createEffect, createRoot, createSignal, on, onMount } from "solid-js";
+import { isServer } from "solid-js/web";
 
 const [_mobileLayout, setMobileLayout] = createSignal(false);
-const query = createMediaQuery("(max-width: 1100px)");
 
-createRoot(() => {
-	createEffect(on(query, (q) => setMobileLayout(q), { defer: true }));
-});
+if (isServer) {
+	const query = createMediaQuery("(max-width: 1100px)");
 
-setTimeout(() => setMobileLayout(query()));
+	createRoot(() => {
+		createEffect(on(query, (q) => setMobileLayout(q), { defer: true }));
+	});
+
+	setTimeout(() => setMobileLayout(query()));
+}
 
 export const mobileLayout = _mobileLayout;
