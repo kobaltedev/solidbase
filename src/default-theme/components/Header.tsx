d48158faf4e2d042868daeef3629ea4bd6630afb
jer3m01 <jer3m01@jer3m01.com>
2025-05-23 18:42:05 +0200

    feat: mobile nav menu (#91)

--- src/default-theme/components/Header.tsx
+++ src/default-theme/components/Header.tsx
@@ -2,10 +2,13 @@ import { Dialog } from "@kobalte/core/dialog";
 import { useMatch } from "@solidjs/router";
 import { For, Show, createSignal, lazy } from "solid-js";
 
+import IconArrowDownLine from "~icons/ri/arrow-down-s-line";
+import IconCloseFill from "~icons/ri/close-large-fill";
+import IconMenuLeftLine from "~icons/ri/menu-2-line";
+import IconMenuFill from "~icons/ri/menu-fill";
 import { getLocaleLink, useCurrentPageData, useLocale } from "../../client";
 import { useDefaultThemeComponents, useDefaultThemeState } from "../context";
 import { useRouteConfig } from "../utils";
-import { ArrowDownIcon, MenuLeftIcon } from "./icons";
 
 import { useSidebar } from "../sidebar";
 import styles from "./Header.module.css";
@@ -14,12 +17,19 @@ const DocSearch = lazy(() => import("./DocSearch"));
 
 export default function Header() {
 	const [tocRef, setTocRef] = createSignal<HTMLElement>();
+	const [navRef, setNavRef] = createSignal<HTMLElement>();
 
 	const { ThemeSelector, LocaleSelector, TableOfContents } =
 		useDefaultThemeComponents();
 
-	const { tocOpen, setTocOpen, setSidebarOpen, frontmatter } =
-		useDefaultThemeState();
+	const {
+		tocOpen,
+		setTocOpen,
+		setSidebarOpen,
+		frontmatter,
+		navOpen,
+		setNavOpen,
+	} = useDefaultThemeState();
 
 	const config = useRouteConfig();
 	const locale = useLocale();
@@ -34,83 +44,132 @@ export default function Header() {
 		frontmatter()?.toc !== false && tocContent() && tocContent()!.length > 0;
 
 	return (
-		<Dialog open={tocOpen()} onOpenChange={setTocOpen} modal={false}>
-			<header class={styles.header}>
-				<div>
-					<a
-						href={getLocaleLink(locale.currentLocale())}
-						class={styles["logo-link"]}
-					>
-						<Show when={config().logo} fallback={<span>{config().title}</span>}>
-							<img src={config().logo} alt={config().title} />
-						</Show>
-					</a>
-					<div class={styles.selectors}>
-						{config().themeConfig?.search?.provider === "algolia" && (
-							<DocSearch />
-						)}
-						<Show when={config().themeConfig?.nav}>
-							{(nav) => (
-								<For each={nav()}>
-									{(item) => {
-										const match = useMatch(() =>
-											locale.applyPathPrefix(
-												`${item.activeMatch ?? item.link}/*rest`,
-											),
-										);
+		<header class={styles.header}>
+			<div>
+				<a
+					href={getLocaleLink(locale.currentLocale())}
+					class={styles["logo-link"]}
+				>
+					<Show when={config().logo} fallback={<span>{config().title}</span>}>
+						<img src={config().logo} alt={config().title} />
+					</Show>
+				</a>
+				<div class={styles["top-nav"]}>
+					<Dialog open={navOpen()} onOpenChange={setNavOpen} modal={false}>
+						<Dialog.Trigger
+							type="button"
+							class={styles["mobile-nav-menu"]}
+							aria-label="Open navigation"
+						>
+							<IconMenuFill class={styles["menu-icon"]} />
+							<IconCloseFill class={styles["close-icon"]} />
+						</Dialog.Trigger>
 
-										return (
-											<a
-												class={styles.navLink}
-												href={locale.applyPathPrefix(item.link)}
-												data-matched={match() !== undefined ? true : undefined}
-											>
-												{item.text}
-											</a>
-										);
-									}}
-								</For>
-							)}
-						</Show>
-						<LocaleSelector />
-						<ThemeSelector />
-					</div>
+						<Dialog.Portal mount={navRef()}>
+							<Dialog.Content class={styles["nav-popup"]}>
+								<Show when={config().themeConfig?.nav}>
+									{(nav) => (
+										<For each={nav()}>
+											{(item) => {
+												const match = useMatch(() =>
+													locale.applyPathPrefix(
+														`${item.activeMatch ?? item.link}/*rest`,
+													),
+												);
+
+												return (
+													<a
+														class={styles.navLink}
+														href={locale.applyPathPrefix(item.link)}
+														data-matched={
+															match() !== undefined ? true : undefined
+														}
+														onClick={() => setNavOpen(false)}
+													>
+														{item.text}
+													</a>
+												);
+											}}
+										</For>
+									)}
+								</Show>
+								<div class={styles["nav-popup-selectors"]}>
+									<LocaleSelector />
+									<ThemeSelector />
+								</div>
+							</Dialog.Content>
+						</Dialog.Portal>
+					</Dialog>
+					{config().themeConfig?.search?.provider === "algolia" && (
+						<DocSearch />
+					)}
+					<Show when={config().themeConfig?.nav}>
+						{(nav) => (
+							<For each={nav()}>
+								{(item) => {
+									const match = useMatch(() =>
+										locale.applyPathPrefix(
+											`${item.activeMatch ?? item.link}/*rest`,
+										),
+									);
+
+									return (
+										<a
+											class={styles.navLink}
+											href={locale.applyPathPrefix(item.link)}
+											data-matched={match() !== undefined ? true : undefined}
+										>
+											{item.text}
+										</a>
+									);
+								}}
+							</For>
+						)}
+					</Show>
+					<LocaleSelector />
+					<ThemeSelector />
 				</div>
-				<Show when={hasSidebar() || hasToc()}>
-					<div class={styles["mobile-bar"]}>
-						<Show when={hasSidebar()} fallback={<div />}>
-							<button
-								type="button"
-								class={styles["mobile-menu"]}
-								onClick={() => setSidebarOpen((p) => !p)}
-								aria-label="Open navigation"
-							>
-								<MenuLeftIcon /> Menu
-							</button>
-						</Show>
+			</div>
+
+			<div ref={setNavRef} class={styles["nav-container"]} />
+
+			<Show when={hasSidebar() || hasToc()}>
+				<div class={styles["mobile-bar"]}>
+					<Show when={hasSidebar()} fallback={<div />}>
+						<button
+							type="button"
+							class={styles["mobile-menu"]}
+							onClick={() => setSidebarOpen((p) => !p)}
+							aria-label="Open navigation"
+						>
+							<IconMenuLeftLine /> Menu
+						</button>
+					</Show>
+
+					<Dialog open={tocOpen()} onOpenChange={setTocOpen} modal={false}>
 						<Show when={hasToc()}>
 							<Dialog.Trigger
 								type="button"
 								class={styles["mobile-menu"]}
 								aria-label="Open table of contents"
 							>
-								On this page <ArrowDownIcon />
+								On this page <IconArrowDownLine />
 							</Dialog.Trigger>
 						</Show>
-					</div>
-				</Show>
 
-				<div ref={setTocRef} class={styles["toc-container"]} />
-			</header>
+						<Dialog.Portal mount={tocRef()}>
+							<Dialog.Content
+								class={styles["toc-popup"]}
+								onClick={() => setTocOpen(false)}
+							>
+								<TableOfContents />
+							</Dialog.Content>
+						</Dialog.Portal>
+					</Dialog>
+				</div>
+			</Show>
 
-			<Dialog.Portal mount={tocRef()}>
-				<Dialog.Content
-					class={styles["toc-popup"]}
-					onClick={() => setTocOpen(false)}
-				>
-					<TableOfContents />
-				</Dialog.Content>
-			</Dialog.Portal>
-		</Dialog>
+			<div ref={setTocRef} class={styles["toc-container"]} />
+		</header>
 	);
 }
--- src/default-theme/components/icons.tsx
+++ src/default-theme/components/icons.tsx
@@ -2,19 +2,6 @@
 
 import type { ComponentProps } from "solid-js";
 
-export function MenuIcon(props: ComponentProps<"svg">) {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 24 24"
-			fill="currentColor"
-			{...props}
-		>
-			<path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z" />
-		</svg>
-	);
-}
-
 export function ArrowDownIcon(props: ComponentProps<"svg">) {
 	return (
 		<svg
