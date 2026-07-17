6d151fb4ff8b2b7bd13f1acc1a09d7e5ac22e383
jer3m01 <jer3m01@jer3m01.com>
2026-07-17 14:49:59 +0200

    ref

--- src/default-theme/components/vendor/DocSearch.tsx
+++ src/default-theme/components/vendor/DocSearch.tsx
@@ -1,33 +1,24 @@
 import docsearch, { type DocSearchProps } from "@docsearch/js";
-import { onMount } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
 
 import "@docsearch/css/dist/style.css";
 import styles from "./DocSearch.module.css";
 
 export default function DocSearch(props: {
-	docsearch: Omit<DocSearchProps, "container"> & { container?: string };
+	docsearch: Omit<DocSearchProps, "container">;
 }) {
-	const id = props.docsearch.container ?? "docsearch";
+	const [ref, setRef] = createSignal<HTMLElement>();
 
-	onMount(() => {
+	createEffect(() => {
+		if (!ref()) return;
 		try {
 			//@ts-expect-error: docsearch not callable?
 			docsearch({
-				container: `#${id}`,
+				container: ref(),
 				...props.docsearch,
 			});
-		} catch {
-			try {
-				setTimeout(() => {
-					//@ts-expect-error: docsearch not callable?
-					docsearch({
-						container: `#${id}`,
-						...props.docsearch,
-					});
-				}, 100);
-			} catch {}
-		}
+		} catch {}
 	});
 
-	return <div id={id} class={styles.docsearch} />;
+	return <div ref={setRef} class={styles.docsearch} />;
 }
